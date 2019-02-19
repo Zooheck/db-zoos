57e7153fb004af72945a233272488e948e6842fa
Zooheck <davidscottflack@gmail.com>
2019-02-19 16:26:00 -0600

    Edit zoos

--- index.js
+++ index.js
@@ -64,7 +64,24 @@ server.post('/api/zoos', async (req, res) => {
   }
 });
 
-
+server.put('/api/zoos/:id', async (req, res) => {
+  try {
+    const count = await db('zoos')
+      .where({ id: req.params.id })
+      .update(req.body)
+    if(count > 0) {
+      const zoo = await db('zoos')
+        .where({ id: req.params.id })
+        .first()
+      res.status(200).json(zoo)
+    } else {
+      res.status(404).json({message: 'Zoo not found'})
+    }
+  }
+  catch(err) {
+    res.status(500).json(err)
+  }
+});
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
